81a627946d722b12ea45b91d2252bc1212a7b34e
Salma22ch <57417762+Salma22ch@users.noreply.github.com>
2021-06-14 12:38:28 +0100

    credit secreen -version 2

--- lib/screens/credit_screen.dart
+++ lib/screens/credit_screen.dart
@@ -29,18 +29,19 @@ class _CreditScreenState extends State<CreditScreen> {
             SizedBox(
               height: 5,
             ),
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  color: Palette.grey1Color,
-                  borderRadius: BorderRadius.circular(10),
-                ),),
-            ),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+             children: [
+              topcards(),
+               SizedBox(
+                 width: 20,
+               ),
+               topcards()
+             ],
+           ),
+
             SizedBox(
-              height: 5,
+              height: 25,
             ),
             Row(
               children: [
@@ -49,22 +50,78 @@ class _CreditScreenState extends State<CreditScreen> {
                   style: Styles.headingStyle,
                   textAlign: TextAlign.start,
                 ),
+                Spacer(),
+                FloatingActionButton.extended(
+                  onPressed: () {
+                    showModalBottomSheet(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        context: context,
+                        builder: (context) {
+                          return Container(
+                            decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: new BorderRadius.only(
+                                    topLeft: const Radius.circular(10.0),
+                                    topRight: const Radius.circular(10.0))),
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                ListTile(
+                                  leading: new Icon(Icons.account_balance_wallet_rounded),
+                                  title: new Text('+/-'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.account_box),
+                                  title: new Text('nom du client'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.monetization_on),
+                                  title: new Text('montant'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                                ListTile(
+                                  leading: new Icon(Icons.date_range),
+                                  title: new Text('date'),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                  },
+                                ),
+                              ],
+                            ),
+                          );
+                        });
+                  },
+                  backgroundColor:Palette.primaryLightColor,
+                 // icon: Icon(Icons.account_circle),
+                  label: Text("Ajouter un client"),
+                ),
               ],
             ),
             SizedBox(
-              height: 5,
+              height: 15,
             ),
 
-         Container(
-           height: 500,
-           child: ListView.builder(
-           scrollDirection: Axis.vertical,
-          padding: EdgeInsets.only(left:16 , right:6),
-          itemCount:7 ,
-            itemBuilder:(context,index){
-               return creditcard();
-             }
-           )
+         Expanded(
+           child: Container(
+             child: ListView.builder(
+             scrollDirection: Axis.vertical,
+            padding: EdgeInsets.only(right:10),
+            itemCount:17 ,
+              itemBuilder:(context,index){
+                 return creditcard();
+               }
+             )
+           ),
          )
 
 
@@ -86,67 +143,115 @@ class creditcard extends StatefulWidget {
 class _creditcardState extends State<creditcard> {
   @override
   Widget build(BuildContext context) {
-    return    Row(
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return    Column(
       children: [
-        Container(
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-              color: Colors.grey.withOpacity(0.1) ,
-              shape :BoxShape.circle
-          ),
-          child: Center(
-            child: FlutterLogo(),
-          ),
-        ),
-        SizedBox(
-          width: 15,
-        ),
-        Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "MOHAMMED ALAMI",
-                style: TextStyle(
-                    fontSize : 15 ,
-                    color : Colors.black,
-                    fontWeight: FontWeight.bold
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.1) ,
+                    shape :BoxShape.circle
+                ),
+                child: Center(
+                  child: FlutterLogo(),
                 ),
               ),
-              Text(
-                " 20/02/2019",
-                style: TextStyle(
-                    fontSize : 15,
-                    color : Colors.black.withOpacity(0.5),
-                    fontWeight: FontWeight.w200
+            ),
+
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "MOHAMMED ALAMI",
+                    style: TextStyle(
+                        fontSize : 15 ,
+                        color : Colors.black,
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  Text(
+                    " 20/02/2019",
+                    style: TextStyle(
+                        fontSize : 15,
+                        color : Colors.black.withOpacity(0.5),
+                        fontWeight: FontWeight.w200
+                    ),
+                  ),
+
+                ],
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      " 245 DH",
+                      style: TextStyle(
+                          fontSize : 15,
+                          color : Colors.red,
+                          fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ],
                 ),
+
               ),
 
-            ],
-          ),
+            )
+          ],
+
         ),
-        Spacer(),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  " 245 DH",
-                  style: TextStyle(
-                      fontSize : 15,
-                      color : Colors.red,
-                      fontWeight: FontWeight.w600
-                  ),
-                ),
-              ],
-            ),
-          ),
-        )
+        Divider()
       ],
+    );
+  }
+}
+
+class topcards extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: Container(
+        padding: const EdgeInsets.all(30),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Palette.grey1Color ,
+                blurRadius: 5,
+                offset: Offset(2, 2), // Shadow position
+              ),]
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Payé',
+              style: TextStyle(
+                  color: Palette.grey2Color,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w600),
+            ),
+            Text(
+              '4070 DH',
+              style: TextStyle(
+                  color: Palette.primaryLightColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 }
