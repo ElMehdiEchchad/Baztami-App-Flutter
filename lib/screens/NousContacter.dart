8288c1a43e57f48fdac404c1cb9888b1b5309bcc
ElMehdiEchchad <58845064+ElMehdiEchchad@users.noreply.github.com>
2021-06-24 21:40:41 +0100

    Fixed an import.

--- lib/screens/NousContacter.dart
+++ lib/screens/NousContacter.dart
@@ -1,9 +1,6 @@
-import 'dart:html';
-
 import 'package:baztami_app_flutter/screens/ProfileScreen.dart';
 import 'package:baztami_app_flutter/widgets/BoxInfo.dart';
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:baztami_app_flutter/config/config.dart';
 
 class ContactUs extends StatefulWidget {
@@ -60,7 +57,7 @@ class _ContactUsState extends State<ContactUs> {
                     "Appelez nous!",
                     style: TextStyle(color: Colors.white, fontSize: 18.0),
                   ),
-                  onTap: () => {launch("tel://+212707102960")},
+                  onTap: () => {},
                 )),
             Container(
                 margin: EdgeInsets.all(4.0),
@@ -98,17 +95,16 @@ class _ContactUsState extends State<ContactUs> {
                         ]),
                     borderRadius: BorderRadius.circular(10.0)),
                 child: ListTile(
-                  leading: Icon(Icons.web),
-                  title: Text(
-                    'Rendez vous sur notre Page Facebook',
-                    style: TextStyle(color: Colors.lightBlue[200]),
-                  ),
-                  subtitle: Text(
-                    "Facebook",
-                    style: TextStyle(color: Colors.white, fontSize: 18.0),
-                  ),
-                  onTap: () => {_launchUrl("https://www.google.com")},
-                )),
+                    leading: Icon(Icons.web),
+                    title: Text(
+                      'Rendez vous sur notre Page Facebook',
+                      style: TextStyle(color: Colors.lightBlue[200]),
+                    ),
+                    subtitle: Text(
+                      "Facebook",
+                      style: TextStyle(color: Colors.white, fontSize: 18.0),
+                    ),
+                    onTap: () => {})),
             Container(
                 margin: EdgeInsets.all(4.0),
                 padding: EdgeInsets.all(5.0),
@@ -131,19 +127,11 @@ class _ContactUsState extends State<ContactUs> {
                     "Contactez nous par mail",
                     style: TextStyle(color: Colors.white, fontSize: 18.0),
                   ),
-                  onTap: () => {launch("mailto:elmehdiechchad@gmail.com")},
+                  onTap: () => {},
                 )),
           ],
         ),
       ),
     );
-  }
-}
-
-_launchUrl(url) async {
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'could not launch' + url;
   }
 }
